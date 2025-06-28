9ad2197e17cf6cbf77e53cbd471a414e0781d45c
Kevin <kimutaimoringa0001@gmail.com>
2025-06-28 15:14:05 +0300

    bug fixes

--- server/app.py
+++ server/app.py
@@ -10,16 +10,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-# server/app.py
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from models import db, Message
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
 CORS(app)
 db.init_app(app)
 
